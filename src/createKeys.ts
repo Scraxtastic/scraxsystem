14c0f7970c4f0e17cbf0765a31613066f17b9c44
Scraxtastic <nick.u19990@gmail.com>
2024-02-23 23:39:45 +0100

    added pizero2 and server keys

--- src/createKeys.ts
+++ src/createKeys.ts
@@ -3,6 +3,8 @@ import { ensureKey } from "./fileManager";
 
 ensureKey(randomBytes(32), "pi4b");
 ensureKey(randomBytes(32), "pizero");
+ensureKey(randomBytes(32), "pizero2");
 ensureKey(randomBytes(32), "invader");
 ensureKey(randomBytes(32), "pixel7");
 ensureKey(randomBytes(32), "mi9");
+ensureKey(randomBytes(32), "server");
