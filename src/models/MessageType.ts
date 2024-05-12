03ff9ca5db14ba243d79cfed293e15f954a294e6
Scraxtastic <nick.u19990@gmail.com>
2024-05-13 01:58:19 +0200

    added modFinished type

--- src/models/MessageType.ts
+++ src/models/MessageType.ts
@@ -1,1 +1,1 @@
-export type MessageType = "error" | "info" | "success" | "data" | "startHandshake" | "mod" | "updateMods";
+export type MessageType = "error" | "info" | "success" | "data" | "startHandshake" | "mod" | "modFinished" | "updateMods";
